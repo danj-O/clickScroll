8889ba14f36128783a37c9d63004a183ff0a3086
danj-O <danjowanjo@gmail.com>
2021-09-03 13:48:04 -0400

    adding express and testing IPs

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const scrapeUtils = require('./scrapeUtils')
 const serverUtils = require('./serverUtils')
 require('dotenv').config();
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+
+//boilerplate
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.set('view engine','ejs');
+app.use(express.urlencoded({
+  extended: true
+}));
+app.listen(PORT, function(){
+  console.log("Server is running on port 3000")
+})
 
 serverUtils.getIP()
 scrapeUtils.clickFarm(['https://www.amazon.com/gp/product/B07KB11SFG', 'https://www.amazon.com/dp/B0711WV63B'])
+
