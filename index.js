1ad79308c46ec55a66e6b201ca38bdc467f5262e
danj-O <danjowanjo@gmail.com>
2021-09-03 11:04:16 -0400

    changing urls

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ const serverUtils = require('./serverUtils')
 require('dotenv').config();
 
 serverUtils.getIP()
-scrapeUtils.clickFarm(['https://www.youtube.com/watch?v=ccVT6c5wMiE&t=219s', 'https://www.youtube.com/watch?v=ccVT6c5wMiE&t=219s', 'https://www.youtube.com/watch?v=ccVT6c5wMiE&t=219s'])
-// scrapeUtils.clickFarm(['https://www.amazon.com/gp/product/B07KB11SFG', 'https://www.amazon.com/dp/B0711WV63B'])
+scrapeUtils.clickFarm(['https://www.amazon.com/gp/product/B07KB11SFG', 'https://www.amazon.com/dp/B0711WV63B'])
